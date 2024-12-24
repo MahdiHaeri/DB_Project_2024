9e03e807af8b9e11de8a6b02b6086a81d51eb446
Mahdi Haeri <mahdihaerim@gmail.com>
2024-12-24 17:14:17 +0330

    Initial commit

---
+++ src/main/kotlin/org/example/db_project_2024/DbProject2024Application.kt
@@ -0,0 +1,11 @@
+package org.example.db_project_2024
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbProject2024Application
+
+fun main(args: Array<String>) {
+	runApplication<DbProject2024Application>(*args)
+}
---
+++ src/test/kotlin/org/example/db_project_2024/DbProject2024ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.db_project_2024
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DbProject2024ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
