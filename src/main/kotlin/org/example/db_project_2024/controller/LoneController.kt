6f1852286dc1c36d6ccfdcd8e2ce416a98afbef2
Mahdi Haeri <mahdihaerim@gmail.com>
2025-01-09 19:54:38 +0330

    Fix endpoint url

--- src/main/kotlin/org/example/db_project_2024/controller/LoneController.kt
+++ src/main/kotlin/org/example/db_project_2024/controller/LoneController.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/lones")
+@RequestMapping("/v1/loans")
 class LoneController (
 
 ) {
