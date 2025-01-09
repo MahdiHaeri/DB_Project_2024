489d51743a05739af9ecb9f4da51cd3719de4f9e
Mahdi Haeri <mahdihaerim@gmail.com>
2025-01-09 19:18:52 +0330

    Add base of Controllers

---
+++ src/main/kotlin/org/example/db_project_2024/controller/TransactionController.kt
@@ -0,0 +1,12 @@
+package org.example.db_project_2024.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/transaction")
+class TransactionController (
+
+) {
+
+}
