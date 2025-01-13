f95593f3620501c852d45376ce11d6da3743a55a
Mahdi Haeri <mahdihaerim@gmail.com>
2025-01-13 20:22:43 +0330

    Add schema.sql for init tables

--- src/main/kotlin/org/example/db_project_2024/service/TransactionService.kt
+++ src/main/kotlin/org/example/db_project_2024/service/TransactionService.kt
@@ -1,13 +1,11 @@
 package org.example.db_project_2024.service
 
 import org.example.db_project_2024.model.Transaction
-import org.example.db_project_2024.repository.UserRepository
 import org.example.db_project_2024.repository.TransactionRepository
 import org.springframework.stereotype.Service
 
 @Service
 class TransactionService(
-    val userRepository: UserRepository,
     private val transactionRepository: TransactionRepository
 ) {
     fun getTransactions(): List<Transaction> {
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,94 @@
+CREATE TABLE users
+(
+    id           SERIAL PRIMARY KEY,
+    username     VARCHAR(255) NOT NULL,
+    password     VARCHAR(255) NOT NULL,
+    first_name   VARCHAR(255) NOT NULL,
+    last_name    VARCHAR(255) NOT NULL,
+    birthday     DATE         NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    email        VARCHAR(255) NOT NULL,
+    address      VARCHAR(255) NOT NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transactions
+(
+    id          SERIAL PRIMARY KEY,
+    sender_id   INT            NOT NULL, -- foreign key
+    receiver_id INT            NOT NULL,
+    value       DECIMAL(10, 2) NOT NULL,
+    type        VARCHAR(255)   NOT NULL,
+    status      VARCHAR(255)   NOT NULL, -- pending, completed, failed
+    open_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    close_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_sender_id FOREIGN KEY (sender_id) REFERENCES users (id)
+);
+
+CREATE TABLE employees
+(
+    id          SERIAL PRIMARY KEY,
+    user_id     INT          NOT NULL, -- foreign key
+    employee_id INT          NOT NULL,
+    job_title   VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE customers
+(
+    id          SERIAL PRIMARY KEY,
+    user_id     INT          NOT NULL, -- foreign key
+    customer_id INT          NOT NULL,
+    type        VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE loans
+(
+    id             SERIAL PRIMARY KEY,
+    customer_id    INT            NOT NULL, -- foreign key
+    type           VARCHAR(255)   NOT NULL,
+    amount         DECIMAL(10, 2) NOT NULL,
+    interest_rate  DECIMAL(10, 2) NOT NULL,
+    payment_period VARCHAR(255)   NOT NULL,
+    start_date     DATE           NOT NULL,
+    end_date       DATE           NOT NULL,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers (id)
+);
+
+CREATE TABLE installments
+(
+    id            SERIAL PRIMARY KEY,
+    loan_id       INT            NOT NULL, -- foreign key
+    due_date      DATE           NOT NULL,
+    payment_date  DATE           NOT NULL,
+    amount_due    DECIMAL(10, 2) NOT NULL,
+    amount_paid   DECIMAL(10, 2) NOT NULL,
+    interest_paid DECIMAL(10, 2) NOT NULL,
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_loan_id FOREIGN KEY (loan_id) REFERENCES loans (id)
+);
+
+CREATE TABLE accounts
+(
+    id            SERIAL PRIMARY KEY,
+    loan_id       INT            NOT NULL, -- foreign key
+    due_date      DATE           NOT NULL,
+    payment_date  DATE           NOT NULL,
+    amount_due    DECIMAL(10, 2) NOT NULL,
+    amount_paid   DECIMAL(10, 2) NOT NULL,
+    interest_paid DECIMAL(10, 2) NOT NULL,
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_loan_id FOREIGN KEY (loan_id) REFERENCES loans (id)
+);
