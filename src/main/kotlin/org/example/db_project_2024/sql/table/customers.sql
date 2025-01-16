6e52a5d44da34f2e85c513c705e775a86c45cce8
Mahdi Haeri <mahdihaerim@gmail.com>
2025-01-16 19:25:24 +0330

    Delete redundent sql table defination

--- src/main/kotlin/org/example/db_project_2024/sql/table/customers.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE customers (
-    id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL, -- foreign key
-    customer_id INT NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
-);
